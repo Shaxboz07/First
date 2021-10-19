5ff910fa1151459e168cab7549cd89483e460347
satimov shaxboz <shahbozsatimo@gmail.com>
2021-10-19 15:32:04 +0500

    File

---
+++ index.js
@@ -0,0 +1,203 @@
+function ieltsResult() {
+  let inputValue = document.getElementById("ieltsScore").value;
+  if (inputValue === "1") {
+    document.getElementById("ieltsText").innerHTML =
+      "Sizning raqamingiz kichikroq ko`rinadi 🤔";
+    document.getElementById("indexText").innerHTML = "❌";
+  } else if (inputValue === "3") {
+    document.getElementById("ieltsText").innerHTML =
+      "Siz raqamni topdingiz 👏👏👏";
+    document.getElementById("indexText").innerHTML = "3🏆";
+  } else if (inputValue === "2") {
+    document.getElementById("ieltsText").innerHTML =
+      "Sizning raqamingiz noto`g`ri 😮";
+    document.getElementById("indexText").innerHTML = "❌";
+  } else if (inputValue === "4") {
+    document.getElementById("ieltsText").innerHTML =
+      "Sizning raqamingiz kattaroq ko`rinadi 🤔";
+    document.getElementById("indexText").innerHTML = "❌";
+  } else if (inputValue === "5") {
+    document.getElementById("ieltsText").innerHTML =
+      "Sizning raqamingiz noto`g`ri 😫";
+    document.getElementById("indexText").innerHTML = "❌";
+  } else if (inputValue === "6") {
+    document.getElementById("ieltsText").innerHTML =
+      "Sizning raqamingiz kattaroq ko`rinadi 🧐";
+    document.getElementById("indexText").innerHTML = "❌";
+  } else if (inputValue === "7") {
+    document.getElementById("ieltsText").innerHTML =
+      "Sizning raqamingiz kattaroq ko`rinadi 😮";
+    document.getElementById("indexText").innerHTML = "❌";
+  } else if (inputValue === "8") {
+    document.getElementById("ieltsText").innerHTML =
+      "Sizning raqamingiz kattaroq ko`rinadi 🧐";
+    document.getElementById("indexText").innerHTML = "❌";
+  } else if (inputValue === "9") {
+    document.getElementById("ieltsText").innerHTML =
+      "Sizning raqamingiz kattaroq shekilli 🤔";
+    document.getElementById("indexText").innerHTML = "❌";
+  } else if (inputValue === "10") {
+    document.getElementById("ieltsText").innerHTML =
+      "Sizning raqamingiz juda katta ko`rinadi 🧐";
+    document.getElementById("indexText").innerHTML = "❌";
+  } else {
+    document.getElementById("ieltsText").innerHTML =
+      "Bu raqam mavjud emas 1 dan 10gacha raqam yozish mumkin🧩";
+    document.getElementById("indexText").innerHTML = "☠";
+  }
+}
+
+// function bmiResult(){
+//     let mass = document.getElementById("mass").value;
+//     let height = document.getElementById("height").value;
+
+//     let bmi = mass/height**2;
+
+//     if (bmi<=18.5){
+//         document.getElementById("bmiText").innerHTML = "you are underweight!"
+//     }else if(bmi>18.5 && bmi<=24){
+//         document.getElementById("bmiText").innerHTML = "you are healthy!"
+//     }else if(bmi>24 && bmi<=30){
+//         document.getElementById("bmiText").innerHTML = "you are overweight!"
+//     }else if(bmi>30){
+//         document.getElementById("bmiText").innerHTML = "you are obese!"
+//     }
+// }
+
+// bmiResult()
+
+function ieltsResult() {
+  let inputValue = document.getElementById("ieltsScore").value;
+  if (inputValue === "1") {
+    document.getElementById("ieltsText").innerHTML =
+      "Sizning raqamingiz kichikroq ko`rinadi 🤔";
+    document.getElementById("indexText").innerHTML = "❌";
+  } else if (inputValue === "3") {
+    document.getElementById("ieltsText").innerHTML =
+      "Siz raqamni topdingiz 👏👏👏";
+    document.getElementById("indexText").innerHTML = "3🏆";
+  } else if (inputValue === "2") {
+    document.getElementById("ieltsText").innerHTML =
+      "Sizning raqamingiz noto`g`ri 😮";
+    document.getElementById("indexText").innerHTML = "❌";
+  } else if (inputValue === "4") {
+    document.getElementById("ieltsText").innerHTML =
+      "Sizning raqamingiz kattaroq ko`rinadi 🤔";
+    document.getElementById("indexText").innerHTML = "❌";
+  } else if (inputValue === "5") {
+    document.getElementById("ieltsText").innerHTML =
+      "Sizning raqamingiz noto`g`ri 😫";
+    document.getElementById("indexText").innerHTML = "❌";
+  } else if (inputValue === "6") {
+    document.getElementById("ieltsText").innerHTML =
+      "Sizning raqamingiz kattaroq ko`rinadi 🧐";
+    document.getElementById("indexText").innerHTML = "❌";
+  } else if (inputValue === "7") {
+    document.getElementById("ieltsText").innerHTML =
+      "Sizning raqamingiz kattaroq ko`rinadi 😮";
+    document.getElementById("indexText").innerHTML = "❌";
+  } else if (inputValue === "8") {
+    document.getElementById("ieltsText").innerHTML =
+      "Sizning raqamingiz kattaroq ko`rinadi 🧐";
+    document.getElementById("indexText").innerHTML = "❌";
+  } else if (inputValue === "9") {
+    document.getElementById("ieltsText").innerHTML =
+      "Sizning raqamingiz kattaroq shekilli 🤔";
+    document.getElementById("indexText").innerHTML = "❌";
+  } else if (inputValue === "10") {
+    document.getElementById("ieltsText").innerHTML =
+      "Sizning raqamingiz juda katta ko`rinadi 🧐";
+    document.getElementById("indexText").innerHTML = "❌";
+  } else {
+    document.getElementById("ieltsText").innerHTML =
+      "Bu raqam mavjud emas 1 dan 10gacha raqam yozish mumkin🧩";
+    document.getElementById("indexText").innerHTML = "☠";
+  }
+}
+
+// function bmiResult(){
+//     let mass = document.getElementById("mass").value;
+//     let height = document.getElementById("height").value;
+
+//     let bmi = mass/height**2;
+
+//     if (bmi<=18.5){
+//         document.getElementById("bmiText").innerHTML = "you are underweight!"
+//     }else if(bmi>18.5 && bmi<=24){
+//         document.getElementById("bmiText").innerHTML = "you are healthy!"
+//     }else if(bmi>24 && bmi<=30){
+//         document.getElementById("bmiText").innerHTML = "you are overweight!"
+//     }else if(bmi>30){
+//         document.getElementById("bmiText").innerHTML = "you are obese!"
+//     }
+// }
+
+// bmiResult()
+
+function ieltsResult() {
+  let inputValue = document.getElementById("ieltsScore").value;
+  if (inputValue === "1") {
+    document.getElementById("ieltsText").innerHTML =
+      "Sizning raqamingiz kichikroq ko`rinadi 🤔";
+    document.getElementById("indexText").innerHTML = "❌";
+  } else if (inputValue === "3") {
+    document.getElementById("ieltsText").innerHTML =
+      "Siz raqamni topdingiz 👏👏👏";
+    document.getElementById("indexText").innerHTML = "3🏆";
+  } else if (inputValue === "2") {
+    document.getElementById("ieltsText").innerHTML =
+      "Sizning raqamingiz noto`g`ri 😮";
+    document.getElementById("indexText").innerHTML = "❌";
+  } else if (inputValue === "4") {
+    document.getElementById("ieltsText").innerHTML =
+      "Sizning raqamingiz kattaroq ko`rinadi 🤔";
+    document.getElementById("indexText").innerHTML = "❌";
+  } else if (inputValue === "5") {
+    document.getElementById("ieltsText").innerHTML =
+      "Sizning raqamingiz noto`g`ri 😫";
+    document.getElementById("indexText").innerHTML = "❌";
+  } else if (inputValue === "6") {
+    document.getElementById("ieltsText").innerHTML =
+      "Sizning raqamingiz kattaroq ko`rinadi 🧐";
+    document.getElementById("indexText").innerHTML = "❌";
+  } else if (inputValue === "7") {
+    document.getElementById("ieltsText").innerHTML =
+      "Sizning raqamingiz kattaroq ko`rinadi 😮";
+    document.getElementById("indexText").innerHTML = "❌";
+  } else if (inputValue === "8") {
+    document.getElementById("ieltsText").innerHTML =
+      "Sizning raqamingiz kattaroq ko`rinadi 🧐";
+    document.getElementById("indexText").innerHTML = "❌";
+  } else if (inputValue === "9") {
+    document.getElementById("ieltsText").innerHTML =
+      "Sizning raqamingiz kattaroq shekilli 🤔";
+    document.getElementById("indexText").innerHTML = "❌";
+  } else if (inputValue === "10") {
+    document.getElementById("ieltsText").innerHTML =
+      "Sizning raqamingiz juda katta ko`rinadi 🧐";
+    document.getElementById("indexText").innerHTML = "❌";
+  } else {
+    document.getElementById("ieltsText").innerHTML =
+      "Bu raqam mavjud emas 1 dan 10gacha raqam yozish mumkin🧩";
+    document.getElementById("indexText").innerHTML = "☠";
+  }
+}
+
+// function bmiResult(){
+//     let mass = document.getElementById("mass").value;
+//     let height = document.getElementById("height").value;
+
+//     let bmi = mass/height**2;
+
+//     if (bmi<=18.5){
+//         document.getElementById("bmiText").innerHTML = "you are underweight!"
+//     }else if(bmi>18.5 && bmi<=24){
+//         document.getElementById("bmiText").innerHTML = "you are healthy!"
+//     }else if(bmi>24 && bmi<=30){
+//         document.getElementById("bmiText").innerHTML = "you are overweight!"
+//     }else if(bmi>30){
+//         document.getElementById("bmiText").innerHTML = "you are obese!"
+//     }
+// }
+
+// bmiResult()
